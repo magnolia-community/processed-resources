1fc1705ff04d2f501cf59da02b14b67a7a000b6e
Mikaël Geljić <mikael.geljic@magnolia-cms.com>
2018-01-09 13:53:25 +0700

    Update Copyright years to 2018

--- src/main/java/info/magnolia/module/resources/app/action/ShowResourceVersionsActionDefinition.java
+++ src/main/java/info/magnolia/module/resources/app/action/ShowResourceVersionsActionDefinition.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2013-2017 Magnolia International
+ * This file Copyright (c) 2013-2018 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
